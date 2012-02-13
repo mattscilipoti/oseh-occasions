2e7e3d97694b9f1147b8e9ff112f2db0111f765b
Matt Scilipoti <matt@scilipoti.name>
2012-02-13 17:40:55 -0500

    rails new oseh-occasions --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OsehOccasions::Application.config.secret_token = 'ba4182163fd517ac788844b84a58efb1186e93f695c35dbc75fa0ff6b9c1be53632ee65ff7588d3eee8f771873dc884ebfa34b9d9010c71667c85cbb8226d9e4'
