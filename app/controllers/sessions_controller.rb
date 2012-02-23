6e6f195a8d19760f5e65fe38b0538e2f2fb11ab5
Matt Scilipoti <matt@scilipoti.name>
2012-02-22 22:21:32 -0500

    Clean up session#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
   def new
-    if full_name = params[:person][:full_name]
-      person = Person.find_by_full_name(full_name)
-      session[:person_id] = person.id
+    unless person_id = params[:person_id]
+      person_args = params[:person]
+      if person_args
+        if person_args[:id]
+          person_id = person_args[:id]
+        else
+          full_name = person_args[:full_name]
+          person = Person.find_by_full_name(full_name)
+          person_id = person.id
+        end
+      end
     end
+    session[:person_id] = person_id
     redirect_to :back
   end
 end
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe SessionsController do
+  describe "GET new" do
+    before :each do
+      request.env["HTTP_REFERER"] = 'http://karma.dev'
+    end
+
+    it "should assign :person_id when passed in :person" do
+      get :new, {:person => {:id => 111}}
+      session[:person_id].should eq(111.to_s)
+    end
+
+    it "should assign :person_id when passed in :person_id" do
+      get :new, {:person_id => 222}
+      session[:person_id].should eq(222.to_s)
+    end
+
+    it "should assign :person_id when :full_name passed in :person" do
+      person = mock_model(Person, :id => 100, :full_name => 'TEST NAME')
+      Person.should_receive(:find_by_full_name).with('TEST NAME').and_return(person)
+
+      get :new, {:person => {:full_name => person.full_name}}
+      session[:person_id].should eq(person.id)
+    end
+
+  end
+end
