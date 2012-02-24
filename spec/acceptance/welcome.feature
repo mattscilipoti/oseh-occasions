3ef89f2b6580e80dec01f9a99c6dd72f83cc9a8c
Matt Scilipoti <matt@scilipoti.name>
2012-02-23 23:48:21 -0500

    Fix Welcome step

--- spec/acceptance/welcome.feature
+++ spec/acceptance/welcome.feature
@@ -5,4 +5,4 @@ Feature: Welcoming Visitors to our site
   Scenario: The Home Page is the Welcome Page
     When I visit "/"
     Then I should be on the home page
-    And I should see an overview of the High Holidays
+    And I should see "Upcoming Events"
--- spec/steps/welcome_steps.rb
+++ spec/steps/welcome_steps.rb
@@ -1,13 +1,10 @@
-step "I should be on the home page" do
-  current_path = URI.parse(current_url).path
-  current_path.should == '/'
-end
+steps_for :welcome do
+  step "I should be on the home page" do
+    current_path = URI.parse(current_url).path
+    current_path.should == '/'
+  end
 
-
-step "I should see an overview of the High Holidays" do
-  texts = ['High Holidays', 'Oseh Shalom']
-  texts.each do |text|
+  step "I should see :text" do |text|
     page.should have_content(text)
   end
 end
-
