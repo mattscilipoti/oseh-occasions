bfbb816e70bffcee52c5f0960ec62c2a1a1efe67
Matt Scilipoti <matt@scilipoti.name>
2012-02-23 23:33:04 -0500

    Add current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
 
   def index
     if person_id = session[:person_id]
@@ -8,11 +9,16 @@ class ApplicationController < ActionController::Base
   end
 
   def logout
+    @current_user = nil
     session[:person_id] = nil
     redirect_to '/', :notice => "Logged out."
   end
 
   private
+
+  def current_user
+    @current_user ||= Person.find(session[:person_id])
+  end
 
   def login(person_id)
     session[:person_id] = person_id
