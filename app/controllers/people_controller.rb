71232be0de432cb0a02d056599775d83cd5fddb4
Matt Scilipoti <matt@scilipoti.name>
2012-02-28 16:43:10 -0500

    bootstrap: theme people
    rails g bootstrap:themed people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,10 +3,10 @@ class PeopleController < ApplicationController
 
   def index
     @scopes = params[:q] || {}
-    @persons = Person.scoped
+    @people = Person.scoped
     @scopes.each do |scope, args|
       logger.debug("DBG: scoping Person: #{scope} => #{args}")
-      @persons = @persons.send(scope, args)
+      @people = @people.send(scope, args)
     end
 
 #    if @scopes && @people.size == 1
@@ -14,7 +14,7 @@ class PeopleController < ApplicationController
 #      session[:person_id] = @person.id
 #      redirect_to :back, :notice => "We found you, #{@person.first_name}!"
 #    else
-      respond_with @persons
+      respond_with @people
 #    end
   end
 end
