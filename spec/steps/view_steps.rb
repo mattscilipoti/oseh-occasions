52d3a490263e28188da4ca9c5415a6801e3ecf10
Matt Scilipoti <matt@scilipoti.name>
2012-03-01 23:35:24 -0500

    fix welcome feature "Only logged in users can RSVP"

--- spec/steps/view_steps.rb
+++ spec/steps/view_steps.rb
@@ -2,4 +2,8 @@ steps_for :global do
   def submit_form
     page.find("input[type='submit']").click
   end
+
+  step "I should see :content" do |content|
+    page.should have_content(content)
+  end
 end
