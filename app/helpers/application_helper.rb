4bee39f6a3a781335545f5ccc98086abb16d328e
Matt Scilipoti <matt@scilipoti.name>
2012-03-12 19:53:14 -0400

    date: extract date_stamp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  def date_stamp(format, with_time = true)
+    stamp = case format
+    when :long
+      "Sunday, May 1, 2000"
+    when :short
+      "Mon, Sep. 24"
+    else
+      fail "That format (#{format}) is not supported."
+    end
+    with_time ? stamp + " 09:00pm" : stamp
+  end
+
   def flash_class(level)
     case level
     when :success then 'success'
