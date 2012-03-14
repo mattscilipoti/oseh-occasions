7b0247706a4433e401fe8e2943f1b70126be0cee
Matt Scilipoti <matt@scilipoti.name>
2012-03-14 17:35:18 -0400

    event: use 'sub_events' instead of 'children'.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :parent, :class_name => Event.name
   has_and_belongs_to_many :attendees, :class_name => 'Person'
-  has_many :children, :class_name => Event.name, :foreign_key => 'parent_id'
+  has_many :sub_events, :class_name => Event.name, :foreign_key => 'parent_id'
 
   scope :upcoming, lambda {|limit|
     limit ||= 5
