9b6c6bc34be208b34426573ccc5ae4452f4a8ebd
Matt Scilipoti <matt@scilipoti.name>
2012-07-18 00:08:56 -0400

    cucumber: remove steps_for :global

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,20 +1,18 @@
-steps_for :global do
-  step "I should be on the home page" do
-    current_path = URI.parse(current_url).path
-    current_path.should == '/'
-  end
+step "I should be on the home page" do
+  current_path = URI.parse(current_url).path
+  current_path.should == '/'
+end
 
-  step "I visit :path" do |path|
-    visit path
-  end
+step "I visit :path" do |path|
+  visit path
+end
 
-  step 'I visit the :event_name event page' do |event_name|
-    event = Event.find_by_name(event_name)
-    visit event_path(event)
-    @current_event = event
-  end
+step 'I visit the $event_name event page' do |event_name|
+  event = Event.find_by_name(event_name)
+  visit event_path(event)
+  @current_event = event
+end
 
-  step "I visit the home page" do
-    visit '/'
-  end
+step "I visit the home page" do
+  visit '/'
 end
--- features/step_definitions/view_steps.rb
+++ features/step_definitions/view_steps.rb
@@ -1,13 +1,11 @@
-steps_for :global do
-  def submit_form
-    page.find("input[type='submit']").click
-  end
+def submit_form
+  page.find("input[type='submit']").click
+end
 
-  step "I should see :content" do |content|
-    page.should have_content(content)
-  end
+step "I should see $content" do |content|
+  page.should have_content(content)
+end
 
-  step "I should not see :content" do |content|
-    page.should_not have_content(content)
-  end
+step "I should not see $content" do |content|
+  page.should_not have_content(content)
 end
