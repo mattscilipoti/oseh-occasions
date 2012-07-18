ca51f30e6e6b8c9ebd108697269e048cdee1be6f
Matt Scilipoti <matt@scilipoti.name>
2012-07-17 22:03:24 -0400

    event: extract common before for spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,39 +1,27 @@
 require 'spec_helper'
 
 describe Event do
+  before :each do
+    # have 3 household members
+    household = Factory :household, :family_name => "foo"
+    @first_person = Factory :person, :first_name => 'FIRST', :household => household
+    @second_person = Factory :person, :first_name => 'SECOND', :household => household
+    @third_person = Factory :person, :first_name => 'THIRD', :household => household
+
+    # assign all 3
+    subject.attendee_ids = household.member_ids
+    # delete 2nd (or assign 1,3)
+    # forms/controller send ids as strings
+    subject.household_attendee_ids=[@first_person.id.to_s,@third_person.id]
+  end
 
   describe ".household_attendee_ids" do
     it "should delete household members not in list" do
-      # have 3 household members
-      household = Factory :household, :family_name => "foo"
-      first_person = Factory :person, :household => household
-      second_person = Factory :person, :household => household
-      third_person = Factory :person, :household => household
-
-      # assign all 3
-      subject.attendee_ids = household.member_ids
-      # delete 2nd (or assign 1,3)
-      # forms/controller send ids as strings
-      subject.household_attendee_ids=[first_person.id.to_s,third_person.id]
-      subject.attendees.should_not include(second_person)
-
+      subject.attendees.should_not include(@second_person)
     end
 
     it "should add new household members from the list" do
-      # have 3 household members
-      household = Factory :household, :family_name => "foo"
-      first_person = Factory :person, :household => household
-      second_person = Factory :person, :household => household
-      third_person = Factory :person, :household => household
-
-      # assign all 3
-      subject.attendee_ids = household.member_ids
-      # delete 2nd (or assign 1,3)
-      # forms/controller send ids as strings
-      subject.household_attendee_ids=[first_person.id.to_s,third_person.id]
-      subject.attendees.should include(first_person)
-
+      subject.attendees.should =~ [@first_person, @third_person]
     end
-
   end
 end
